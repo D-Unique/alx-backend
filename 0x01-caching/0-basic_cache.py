2acf4a0c4695518f6054130e7ad6d12b0e5ab79a
Unigwe <emmunigwe@gmail.com>
2024-10-31 17:19:53 +0100

    feat(0-basic_cache.py* )

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""The BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Inherits from BaseCaching"""
+    def __init__(self):
+        super().__init__()
+        """call the parent class"""
+
+    def put(self, key, item):
+        """this function adds item to the dict - cache_data
+        """
+        rkey = self.cache_data.get(key)
+        if (rkey is None):
+            self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """this function retrives item from the dict - cache_data
+            using its key
+        """
+        rkey = self.cache_data.get(key)
+        if (rkey is None):
+            return None
+        else:
+            return rkey

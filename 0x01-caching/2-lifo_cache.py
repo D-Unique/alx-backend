e744a60c15d5c39df00557234ad47d34b407adc0
Unigwe <emmunigwe@gmail.com>
2024-10-31 20:04:29 +0100

    fix(2-lifo_cache.py)

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -22,7 +22,7 @@ class LIFOCache(BaseCaching):
 
     def get(self, key):
         """Retrives the value of the key"""
-        if (key is None or key not in self.cache_data.keys):
+        if (key is None or key not in self.cache_data.keys()):
             return None
-
-        return self.cache_data.get(key)
+        else:
+            return self.cache_data.get(key)

2ce80ad1434f58bbb25e386e8bcac29160ac23d4
Unigwe <emmunigwe@gmail.com>
2024-11-06 13:10:30 +0100

    edit 3-index html

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """A basic flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel 
+from flask_babel import _
 
 
 class Config:
